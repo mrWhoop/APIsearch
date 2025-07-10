f6b1b62126c4b20831f74ffe63ee7b865bb1ccdb
Robi <modic.robert@gmail.com>
2025-07-10 09:19:49 +0200

    fixed bug in meilisearch populating routine

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,4 +1,7 @@
+import meilisearch
 from flask import Blueprint, jsonify, request
+from mpmath.calculus.extrapolation import limit
+
 from ..extensions import mongo
 from support_scripts.populate_database import populate_database
 from support_scripts.populate_search_index import populate_search_index
@@ -13,9 +16,23 @@ from openai import OpenAI
 import chromadb
 from chromadb.config import Settings
 import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MEILISEARCH_SECRET = os.environ['MEILISEARCH_SECRET']
+MEILISEARCH_INDEX = os.environ['MEILISEARCH_INDEX']
+
+searchClient = meilisearch.Client('http://127.0.0.1:7700', MEILISEARCH_SECRET)
+searchIndex = searchClient.index(MEILISEARCH_INDEX)
 
 
 main_blueprint = Blueprint('main', __name__)
+
+@main_blueprint.route('/support/populate_search_index')
+def populate_search_index_call():
+    populate_search_index()
+    return jsonify({'status': 'Populating meilisearch done.'})
 
 @main_blueprint.route('/support/populate_database')
 def populate_database_call():
@@ -85,28 +102,59 @@ def search():
     chroma_client = chromadb.PersistentClient(path="dataRAG/chroma")
 
     chroma_GPT_collection = chroma_client.get_collection(name="gptAPI")
-
     chroma_Gemini_collection = chroma_client.get_collection(name="GeminiAPI")
 
     chroma_GPT_collection_meta = chroma_client.get_collection(name="gptAPImeta")
-
     chroma_Gemini_collection_meta = chroma_client.get_collection(name="GeminiAPImeta")
 
     query = request.args.get('q')
+    ai = request.args.get('ai')
 
-    query_GPT_embedding = OpenAI_client.embeddings.create(model="text-embedding-ada-002", input=query).data[0].embedding
-    resultsGPT = chroma_GPT_collection.query(query_embeddings=[query_GPT_embedding], n_results=5)
-    resultsGPTmeta = chroma_GPT_collection_meta.query(query_embeddings=[query_GPT_embedding], n_results=5)
+    if ai == 'GPT':
+        query_GPT_embedding = OpenAI_client.embeddings.create(model="text-embedding-ada-002", input=query).data[0].embedding
+        resultsGPT = chroma_GPT_collection.query(query_embeddings=[query_GPT_embedding], n_results=5)
+        return jsonify({"results": resultsGPT["metadatas"][0]})
 
-    query_Gemini_embedding = genai.embed_content(model=embedding_model_name, content=query)['embedding']
-    resultsGemini = chroma_Gemini_collection.query(query_embeddings=[query_Gemini_embedding], n_results=5)
-    resultsGeminiMeta = chroma_Gemini_collection_meta.query(query_embeddings=[query_Gemini_embedding], n_results=5)
+    elif ai == 'GPTmeta':
+        query_GPT_embedding = OpenAI_client.embeddings.create(model="text-embedding-ada-002", input=query).data[0].embedding
+        resultsGPTmeta = chroma_GPT_collection_meta.query(query_embeddings=[query_GPT_embedding], n_results=5)
+        return jsonify({"results": resultsGPTmeta["metadatas"][0]})
 
-    result = {
-        "GPT_RAG": resultsGPT,
-        "GPT_RAG_meta": resultsGPTmeta,
-        "Gemini_RAG": resultsGemini,
-        "Gemini_RAG_meta": resultsGeminiMeta,
-    }
+    elif ai == 'Gemini':
+        query_Gemini_embedding = genai.embed_content(model=embedding_model_name, content=query)['embedding']
+        resultsGemini = chroma_Gemini_collection.query(query_embeddings=[query_Gemini_embedding], n_results=5)
+        return jsonify({"results": resultsGemini["metadatas"][0]})
 
-    return jsonify({"results": resultsGPT["metadatas"][0]})
+    elif ai == 'Geminimeta':
+        query_Gemini_embedding = genai.embed_content(model=embedding_model_name, content=query)['embedding']
+        resultsGeminiMeta = chroma_Gemini_collection_meta.query(query_embeddings=[query_Gemini_embedding], n_results=5)
+        return jsonify({"results": resultsGeminiMeta["metadatas"][0]})
+
+    elif ai == 'MeiliSearch':
+
+        search_results = searchIndex.search(query, {"limit": 5})['hits']
+
+        print(search_results)
+
+        result = list()
+
+        for sr in search_results:
+
+            print(sr)
+
+            res = {
+                "name": sr['name'],
+                "category": sr['category'],
+                "description": sr['description'],
+                "docs": sr['docs'],
+            }
+
+            result.append(res)
+
+        return jsonify({"results": result})
+
+    else:
+        # this will be normal search, final best product
+        query_Gemini_embedding = genai.embed_content(model=embedding_model_name, content=query)['embedding']
+        resultsGeminiMeta = chroma_Gemini_collection_meta.query(query_embeddings=[query_Gemini_embedding], n_results=5)
+        return jsonify({"results": resultsGeminiMeta["metadatas"][0]})
--- support_scripts/populate_search_index.py
+++ support_scripts/populate_search_index.py
@@ -12,7 +12,26 @@ searchClient = meilisearch.Client('http://127.0.0.1:7700', MEILISEARCH_SECRET)
 searchIndex = searchClient.index(MEILISEARCH_INDEX)
 documents = list()
 
+def cleanUp():
+    try:
+        index = searchClient.index(MEILISEARCH_INDEX)
+
+        # Delete all documents
+        task = index.delete_all_documents()
+        print(f"Deleting all documents from '{MEILISEARCH_INDEX}' index...")
+
+        # Wait for the task to complete
+        searchClient.wait_for_task(task['taskUid'])
+
+        print(f"All documents deleted from '{MEILISEARCH_INDEX}' index successfully!")
+        return True
+    except Exception as e:
+        print(f"Error clearing documents from index '{MEILISEARCH_INDEX}': {e}")
+        return False
+
 def populate_search_index():
+
+    cleanUp()
 
     apis = mongo.db.apis.find()
 
@@ -23,6 +42,7 @@ def populate_search_index():
             'name': api['name'],
             'description': api['description'],
             'category': api['category'],
+            'docs': api['docs'],
             'api_keywords': api['api_keywords'],
         }
         documents.append(doc)
